98d4c0f91ccd24120d363bcb26d0720ce3671cf4
Pavel Kasila <pavel.kasila@gmail.com>
2021-11-05 19:08:19 +0300

    Add sidebar variant for macOS and iPadOS

--- Sources/ModularUI/ModularView.swift
+++ Sources/ModularUI/ModularView.swift
@@ -16,8 +16,23 @@ public struct ModularView: View {
     }
     
     @State private var tabId: String?
-    
+    #if os(iOS)
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    #endif
+
     public var body: some View {
+        #if os(iOS)
+        if horizontalSizeClass == .compact {
+            tabbed
+        } else {
+            sidebar
+        }
+        #else
+        sidebar
+        #endif
+    }
+    
+    private var tabbed: some View {
         TabView(selection: $tabId) {
             ForEach(registry.tabs, id: \.id) { tab in
                 NavigationView {
@@ -27,13 +42,35 @@ public struct ModularView: View {
                     .tabItem { tab.label() }
                     .tag(tab.id)
             }
-        }.onAppear {
-            Runtime.classes(conformTo: TabModule.Type.self).forEach { module in
-                if let m = (module as? TabModule.Type) {
-                    Registry.shared.add(tabModule: m.init())
-                } else {
-                    print("Failed to init \(module)")
+        }
+        .onAppear(perform: self.setUp)
+    }
+    
+    private var sidebar: some View {
+        NavigationView {
+            // The first column is the sidebar.
+            List(registry.tabs, id: \.id) { tab in
+                NavigationLink(destination: tab.render()
+                                .navigationTitle(tab.title)) {
+                    tab.label()
                 }
+            }
+ 
+            // Initial content of the second column.
+            EmptyView()
+ 
+            // Initial content for the third column.
+            Text("Select a category of settings in the sidebar.")
+        }
+        .onAppear(perform: self.setUp)
+    }
+    
+    private func setUp() {
+        Runtime.classes(conformTo: TabModule.Type.self).forEach { module in
+            if let m = (module as? TabModule.Type) {
+                Registry.shared.add(tabModule: m.init())
+            } else {
+                print("Failed to init \(module)")
             }
         }
     }
